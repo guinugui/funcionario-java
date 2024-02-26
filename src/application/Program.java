152993ef966dfa646f8d7d01697899551b1f96a7
Guilherme Ribeiro <guilhermevendaseprodutos@gmail.com>
2024-02-26 09:16:03 -0300

    finalizado programa principal

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,36 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.funcionario;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Digite o nome do funcionario: ");
+		String nome = sc.nextLine();
+		System.out.println("Digite o salario bruto: ");
+		double salario = sc.nextDouble();
+		System.out.println("Digite a taxa combrada: ");
+		double taxa = sc.nextDouble();
+		
+		funcionario fun = new funcionario(nome, salario, taxa);
+		
+		System.out.println("Funcionario: " + fun.getNome() + " $" + fun.salarioBruto());
+		
+		System.out.println("Digite qual a porcentagem de comissao: ");
+		double porcentagem = sc.nextDouble();
+		fun.incremetaSalario(porcentagem);
+		
+		System.out.println("Funcionario atualizado; " + fun);
+		
+		
+		sc.close();
 
 	}
 
--- src/entities/funcionario.java
+++ src/entities/funcionario.java
@@ -5,18 +5,47 @@ public class funcionario {
 	private String nome;
 	private Double salario;
 	private Double taxa;
+
 	public funcionario(String nome, Double salario, Double taxa) {
 		this.nome = nome;
 		this.salario = salario;
 		this.taxa = taxa;
 	}
-	
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(Double salario) {
+		this.salario = salario;
+	}
+
+	public Double getTaxa() {
+		return taxa;
+	}
+
+	public void setTaxa(Double taxa) {
+		this.taxa = taxa;
+	}
+
 	public Double salarioBruto() {
 		return salario - taxa;
 	}
+
 	public void incremetaSalario(Double porcentagem) {
-		double soma = salario * (porcentagem / 100.0);
+		salario += salario * (porcentagem / 100.0);
+		
 	}
-	
-	
+	public String toString() {
+		return nome + ", $ " + String.format("%.2f", salarioBruto());
+		}
+
 }
